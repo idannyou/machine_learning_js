f93250d2a23f97abad25286f25b8e6fcbd65a32d
Danny Ou <idannyou@gmail.com>
2019-10-16 14:30:46 -0400

    Add layers to image model

--- mnist-node/model.js
+++ mnist-node/model.js
@@ -17,6 +17,35 @@
 
 const tf = require('@tensorflow/tfjs');
 
+let model = tf.sequential();
 
+model.add(
+  tf.layers.conv2d({
+    inputShape: [28, 28, 1],
+    filters: 32,
+    kernelSize: 3,
+    activation: 'relu'
+  })
+);
+model.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
+model.add(
+  tf.layers.conv2d({
+    filters: 64,
+    kernelSize: 3,
+    activation: 'relu'
+  })
+);
+model.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
+model.add(tf.layers.flatten());
+// dropout works only on flatten dimension
+model.add(tf.layers.dropout({ rate: 0.25 }));
+model.add(tf.layers.dense({ units: 128, activation: 'relu' }));
+model.add(tf.layers.dropout({ rate: 0.5 }));
+model.add(tf.layers.dense({ units: 10, activation: 'softmax' }));
+model.compile({
+  optimizer: 'adam',
+  loss: 'categoricalCrossentropy',
+  metrics: ['accuracy']
+});
 
 module.exports = model;
