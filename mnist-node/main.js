d5f03e6c018f5b22b686de71c02c595aa1ebdcab
Danny Ou <idannyou@gmail.com>
2019-10-16 14:45:48 -0400

    Add model load path

--- mnist-node/main.js
+++ mnist-node/main.js
@@ -21,32 +21,47 @@ const argparse = require('argparse');
 const data = require('./data');
 const model = require('./model');
 
-async function run(epochs, batchSize, modelSavePath) {
+async function run(epochs, batchSize, modelSavePath, modelLoadPath) {
   await data.loadData();
 
-  const {images: trainImages, labels: trainLabels} = data.getTrainData();
-  model.summary();
+  if (modelLoadPath != null) {
+    const loadedModel = await tf.loadLayersModel(`file://${modelLoadPath}`);
+    loadedModel.compile({
+      loss: 'categoricalCrossentropy',
+      optimizer: 'adam',
+      metrics: ['accuracy']
+    });
+    loadedModel.summary();
 
-  let epochBeginTime;
-  let millisPerStep;
-  const validationSplit = 0.15;
-  const numTrainExamplesPerEpoch =
-      trainImages.shape[0] * (1 - validationSplit);
-  const numTrainBatchesPerEpoch =
-      Math.ceil(numTrainExamplesPerEpoch / batchSize);
-  await model.fit(trainImages, trainLabels, {
-    epochs,
-    batchSize,
-    validationSplit
-  });
+    const { images: testImages, labels: testLabels } = data.getTestData();
+    const evalOutput = loadedModel.evaluate(testImages, testLabels);
 
-  const {images: testImages, labels: testLabels} = data.getTestData();
-  const evalOutput = model.evaluate(testImages, testLabels);
-
-  console.log(
+    console.log(
       `\nEvaluation result:\n` +
-      `  Loss = ${evalOutput[0].dataSync()[0].toFixed(3)}; `+
-      `Accuracy = ${evalOutput[1].dataSync()[0].toFixed(3)}`);
+        `  Loss = ${evalOutput[0].dataSync()[0].toFixed(3)}; ` +
+        `Accuracy = ${evalOutput[1].dataSync()[0].toFixed(3)}`
+    );
+  } else {
+    const { images: trainImages, labels: trainLabels } = data.getTrainData();
+    model.summary();
+
+    const validationSplit = 0.15;
+
+    await model.fit(trainImages, trainLabels, {
+      epochs,
+      batchSize,
+      validationSplit
+    });
+
+    const { images: testImages, labels: testLabels } = data.getTestData();
+    const evalOutput = model.evaluate(testImages, testLabels);
+
+    console.log(
+      `\nEvaluation result:\n` +
+        `  Loss = ${evalOutput[0].dataSync()[0].toFixed(3)}; ` +
+        `Accuracy = ${evalOutput[1].dataSync()[0].toFixed(3)}`
+    );
+  }
 
   if (modelSavePath != null) {
     await model.save(`file://${modelSavePath}`);
@@ -67,11 +82,15 @@ parser.addArgument('--batch_size', {
   type: 'int',
   defaultValue: 128,
   help: 'Batch size to be used during model training.'
-})
+});
 parser.addArgument('--model_save_path', {
   type: 'string',
   help: 'Path to which the model will be saved after training.'
 });
+parser.addArgument('--model_load_path', {
+  type: 'string',
+  help: 'Path to the trained model.'
+});
 const args = parser.parseArgs();
 
-run(args.epochs, args.batch_size, args.model_save_path);
+run(args.epochs, args.batch_size, args.model_save_path, args.model_load_path);
